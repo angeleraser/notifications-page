f3db26f103d70a284fb6d63f6c30afa9c7c33951
Angel Figuera <angeleraser@gmail.com>
2022-09-16 10:24:52 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<div className='app'>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+			necessitatibus quam beatae similique, eum quidem dolores error vitae
+			distinctio harum illo! Hic voluptates exercitationem a veniam, sit nemo
+			magni non?
+		</div>
+	);
+}
+
+export default App;
